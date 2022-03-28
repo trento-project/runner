95de02c1bc7fde28b2da0d6c8992b799720820ca
arbulu89 <xarbulu@suse.com>
2022-03-28 15:50:13 +0200

    Flatten checks catalog ouptut

--- runner/catalog.go
+++ runner/catalog.go
@@ -4,14 +4,13 @@ const (
 	CatalogDestinationFile = "ansible/catalog.json"
 )
 
-type Catalog struct {
-	Checks []*CatalogCheck `json:"checks" binding:"required"`
-}
+type Catalog []*CatalogCheck
 
 type CatalogCheck struct {
 	ID             string `json:"id,omitempty" binding:"required"`
 	Name           string `json:"name,omitempty" binding:"required"`
-	Group          string `json:"group,omitempty" binding:"required"`
+	Group          string `json:"group" binding:"required"`
+	Provider       string `json:"provider" binding:"required"`
 	Description    string `json:"description,omitempty"`
 	Remediation    string `json:"remediation,omitempty"`
 	Implementation string `json:"implementation,omitempty"`
